a434dd21f4ac3f030c4f1ebf65304b15531343d6
fakharcyber <hussainfakhar910@gmail.com>
2024-09-24 07:27:20 +0100

    fixed link

---
+++ asset/js/fetch.js
@@ -0,0 +1,164 @@
+let athletes = [
+    { name: "Arshad Nadeem", dec:"Arshad Nadeem (born January 2, 1997, Mian Channu, Punjab, Pakistan) is the first Pakistani track-and-field athlete to win an Olympic medal—a feat he accomplished with no less than a gold medal in the men's javelin throw event at the 2024 Paris Games", img: "asset/img/atheletes/Arshad-Nadeem.webp" },
+    { name: "Usain Bolt", dec: "Usain Bolt is a retired Jamaican sprinter, widely regarded to be the greatest sprinter of all time. He is the world record holder in the 100 meters, 200 meters, and 4 × 100 meters relay.", img: "asset/img/atheletes/300890817-155557270479663-493078166280071531-n-800x448.jpg" },
+    { name: "Simone Biles", dec: "Biles stormed the gymnastics scene when she won five medals at the 2016 Olympic Games in Rio: gold in the team event, the individual all-around competition, the vault and the floor exercise. She also took home the bronze in the balance beam final. The athlete set an American record for most gold medals won at a single Games.", img: "asset/img/atheletes/Simone-Biles-Then-Olympic-Athletes-Where-Are-They-Now.jpg" },
+    { name: "Lindsey Vonn", dec: "Skier Vonn, who made her Olympic debut in 2002 at age 17, was the first American woman to nab a gold medal in the downhill event at the 2010 Winter Olympics. (She also took home a bronze in 2010's Super-G race.)", img: "asset/img/atheletes/Lindsey-Vonn-Then-97140588.jpg" },
+    { name: "Michael Jordan ", dec: "Michael Jordan is an American businessman and former professional basketball player. He played fifteen seasons in the National Basketball Association (NBA), winning six NBA championships with the Chicago Bulls.", img: "asset/img/atheletes/michael-jordan.jpg" },
+    { name: "Muhammad Ali", dec: "Muhammad Ali was an American professional boxer and activist. Nicknamed “The Greatest”, he is widely considered one of the most important and renowned sports figures of the 20th century", img: "asset/img/atheletes/muhammad-ali-435-1-3a9eced7720841e9ae8f2b7448b14f7b.jpg" },
+    { name: "Wayne Gretzky", dec: "Wayne Gretzky is a Canadian former professional ice hockey player and former head coach. He played 20 seasons in the National Hockey League (NHL) for four teams from 1979 to 1999.", img: "asset/img/atheletes/edmonton-wayne-gretzky-031516-getty-ftrjpg_btjzlzxrrpec1covu0vtbck2u.jpg" },
+    { name: "Roger Federer", dec: "Roger Federer is a Swiss professional tennis player. He was rated world No. 1 by the Association of Tennis Professionals for 310 weeks, including a record 237 consecutive weeks, and has spent as the year-end No. 1 five times.He has won 103 ATP singles titles, the second-most of all time after Jimmy Connors, including 20 Grand Slam titles, a record eighth men’s singles Wimbledon titles, and a record six year-end championships.", img: "asset/img/atheletes/309059710-1144542213122630-558252689369347420-n-768x513.jpg" },
+    { name: "Barbara Reed", dec: "Indoors, the Swedish 24-year-old added a centimetre to his previous outright best, clearing 6.22m in Clermont-Ferrand. During the outdoor season, he secured his second consecutive world title in Budapest and then won his third Wanda Diamond League trophy with a clearance of 6.23m on his first attempt to better his world record by another centimetre.", img: "asset/img/atheletes/f.txt.jpg" }
+]
+
+let games = [
+    { title: "Acrobatic Gymnastics", pic: "asset/img/Games/Acrobatic Gymnastics.png" },
+    { title: "Alpine Skiing", pic: "asset/img/Games/Alpine Skiing.svg" },
+    { title: "Archery", pic: "asset/img/Games/Archery.svg" },
+    { title: "Artistic Swimming", pic: "asset/img/Games/Artistic Swimming.svg" },
+    { title: "Athletics", pic: "asset/img/Games/Athletics.svg" },
+    { title: "Badminton", pic: "asset/img/Games/Badminton.svg" },
+    { title: "Beach Volleyball", pic: "asset/img/Games/Beach Volleyball.svg" },
+    { title: "Boxing", pic: "asset/img/Games/Boxing.svg" },
+    { title: "Breaking", pic: "asset/img/Games/Breaking.svg" },
+    { title: "Fencing", pic: "asset/img/Games/Fencing.svg" },
+    { title: "Figure Skating", pic: "asset/img/Games/Figure Skating.svg" },
+    { title: "Football", pic: "asset/img/Games/Football.svg" },
+    { title: "Freestyle Skiing", pic: "asset/img/Games/Freestyle Skiing.svg" },
+    { title: "Hockey", pic: "asset/img/Games/Hockey.svg" },
+    { title: "Judo", pic: "asset/img/Games/Judo.svg" },
+    { title: "Karate", pic: "asset/img/Games/Karate.svg" },
+    { title: "Rhythmic Gymnastics", pic: "asset/img/Games/Rhythmic Gymnastics.svg" },
+    { title: "Roller Skating", pic: "asset/img/Games/Roller speed Skating.svg" },
+    { title: "Rowing", pic: "asset/img/Games/Rowing.svg" },
+    { title: "Sailing", pic: "asset/img/Games/Sailing.svg" },
+    { title: "Shooting", pic: "asset/img/Games/Shooting.svg" },
+    { title: "Jumping", pic: "asset/img/Games/Ski Jumping.svg" },
+];
+
+let gallery = [
+    { img: "asset/img/453899741_18447603832065324_927665523409569235_n.jpg", alt: "" },
+    { img: "asset/img/454035144_18447603841065324_7822349421405304292_n.jpg", alt: "" },
+    { img: "asset/img/454209072_18447603865065324_5329159756508384821_n.jpg", alt: "" },
+    { img: "asset/img/alpine skiing.jpg", alt: "" },
+    { img: "asset/img/archery.jpg", alt: "" },
+    { img: "asset/img/artistic swimming.jpg", alt: "" },
+    { img: "asset/img/athletics 1.jpg", alt: "" },
+    { img: "asset/img/athletics 2.jpg", alt: "" },
+    { img: "asset/img/athletics 3.jpg", alt: "" },
+    { img: "asset/img/badminton.jpg", alt: "" },
+    { img: "asset/img/beach volly ball.jpg", alt: "" },
+    { img: "asset/img/boxing.jpg", alt: "" },
+    { img: "asset/img/fencing.jpg", alt: "" },
+    { img: "asset/img/figure skating.jpg", alt: "" },
+    { img: "asset/img/football.jpg", alt: "" },
+    { img: "asset/img/freestyle skiing.jpg", alt: "" },
+    { img: "asset/img/hockey.jpg", alt: "" },
+    { img: "asset/img/judo.jpg", alt: "" },
+    { img: "asset/img/karate.jpg", alt: "" },
+    { img: "asset/img/OIP (1).jpeg", alt: "" },
+    { img: "asset/img/R.jpeg", alt: "" },
+    { img: "asset/img/rollar skating.jpg", alt: "" },
+    { img: "asset/img/Rowing.jpg", alt: "" },
+    { img: "asset/img/Sailing.jpg", alt: "" }
+]
+const olympicRecords = [
+    {
+        sport: "Track-and-field ",
+        athleteName: "Arshad Nadeem",
+        country: "Pakistan",
+        event: "Javelin throw",
+        record: "92.97 meters",
+        year: 2024,
+        olympicGames: "Paris 2024"
+    },
+    {
+        sport: "Athletics",
+        athleteName: "Usain Bolt",
+        country: "Jamaica",
+        event: "100m Sprint",
+        record: "9.58 sec",
+        year: 2008,
+        olympicGames: "Beijing 2008"
+    },
+    {
+        sport: "Athletics",
+        athleteName: "Usain Bolt",
+        country: "Jamaica",
+        event: "200m Sprint",
+        record: "19.19 sec",
+        year: 2008,
+        olympicGames: "Beijing 2008"
+    },
+    {
+        sport: "Swimming",
+        athleteName: "Michael Phelps",
+        country: "USA",
+        event: "200m Butterfly",
+        record: "1:51.51",
+        year: 2008,
+        olympicGames: "Beijing 2008"
+    },
+    {
+        sport: "Swimming",
+        athleteName: "Katie Ledecky",
+        country: "USA",
+        event: "800m Freestyle",
+        record: "8:04.79",
+        year: 2016,
+        olympicGames: "Rio de Janeiro 2016"
+    },
+    {
+        sport: "Gymnastics",
+        athleteName: "Nadia Comăneci",
+        country: "Romania",
+        event: "Balance Beam",
+        record: "Perfect 10",
+        year: 1976,
+        olympicGames: "Montreal 1976"
+    },
+    {
+        sport: "Weightlifting",
+        athleteName: "Lasha Talakhadze",
+        country: "Georgia",
+        event: "Super-Heavyweight (Total)",
+        record: "484 kg",
+        year: 2020,
+        olympicGames: "Tokyo 2020"
+    },
+    {
+        sport: "Long Jump",
+        athleteName: "Bob Beamon",
+        country: "USA",
+        event: "Men’s Long Jump",
+        record: "8.90 m",
+        year: 1968,
+        olympicGames: "Mexico City 1968"
+    },
+    {
+        sport: "Pole Vault",
+        athleteName: "Sergey Bubka",
+        country: "USSR",
+        event: "Men’s Pole Vault",
+        record: "6.15 m",
+        year: 1988,
+        olympicGames: "Seoul 1988"
+    },
+    {
+        sport: "Javelin Throw",
+        athleteName: "Jan Železný",
+        country: "Czech Republic",
+        event: "Men’s Javelin Throw",
+        record: "98.48 m",
+        year: 1996,
+        olympicGames: "Atlanta 1996"
+    },
+    {
+        sport: "High Jump",
+        athleteName: "Javier Sotomayor",
+        country: "Cuba",
+        event: "Men’s High Jump",
+        record: "2.45 m",
+        year: 1992,
+        olympicGames: "Barcelona 1992"
+    }
+];
